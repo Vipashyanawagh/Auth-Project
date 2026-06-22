388aec2f34b72af650009b9c680d14961e6dde09
Vipashyanawagh <vipashyana.wagh2023@sait.ac.in>
2026-06-22 14:59:05 +0530

    Second commit

---
+++ backend/Routes/AuthRouter.js
@@ -0,0 +1,10 @@
+const router  = require("express").Router();
+const { signupValidation , loginValidation } = require("../Middlewares/AuthValidation");
+const {signup , login} =require("../controllers/AuthController");
+
+
+
+router.post("/signup", signupValidation ,signup);
+router.post("/login", loginValidation, login);
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+//q9xa76BhIhg6U2Is password for mongodb atlas
+
+//mongodb+srv://vipashyanawagh14_db_user:<db_password>@cluster0.92pgfdn.mongodb.net/?appName=Cluster0
+
+
+
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./Routes/AuthRouter");
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/auth", AuthRouter);
+
+require("dotenv").config();
+require("./Models/db");
+const PORT = process.env.PORT || 8080;
+
+app.get("/ping", (req,res) =>{
+    res.send("Pong");
+});
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on ${PORT}`);
+})
